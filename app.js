5b22a5911b03805a8bc992272b128a5e4517193d
Herbert Vojčík <herby@mailbox.sk>
2014-10-18 23:45:04 +0200

    separate codemirrors in amber and helios:
     - as they use old version (3.x), use name codemirror3;
     - they both have their own codemirror3.amd.json;
     - they both load pieces of CM locally, no reuse;
     - legacy IDE not loaded into Helios window - to promote
       CodeMirror upgrade and preventing clashes.

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ define([
 	'amber/helpers', // pre-fetch, dep of amber/deploy
 	'amber/deploy', // pre-fetch, dep of amber/lang
 	'amber/lang',
-	'amber/codemirror',
+    'codemirror3/lib/codemirror',
+    'codemirror3/mode/smalltalk/smalltalk',
+    'codemirror3/addon/hint/show-hint',
+    'css!codemirror3/theme/ambiance',
+    'css!codemirror3/lib/codemirror',
+    'css!codemirror3/addon/hint/show-hint',
     'jquery-ui',
-    'amber_core/IDE',
     'amber_core/Examples',
     'amber_core/Benchfib',
     'bootstrap2.3.2/js/bootstrap',

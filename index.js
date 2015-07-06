b556c386c0d01d0bc0ebd839944deee8dddb373a
Herbert Vojčík <herby@mailbox.sk>
2015-07-06 20:36:20 +0200

    Use hash to pass config path. Fix #67.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ define(["require"], function (require) {
         pageUrl: function () {
             var a = document.createElement("a");
             a.href = require.toUrl("..");
-            return require.toUrl('./index.html') + "?" + encodeURIComponent(a.href);
+            return require.toUrl('./index.html') + "#" + encodeURIComponent(a.href);
         },
         popup: function () {
             window.open(this.pageUrl(), "Helios", "menubar=no, width=1000, height=600");
